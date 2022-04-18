23b15474df3eb3cecb2b81c5fcfea6aeafba5d2f
vladborza <vlad.borza@yahoo.com>
2022-04-18 22:15:33 +0200

    TO DO APDLC format

--- src/groundComms/GroundComms.java
+++ src/groundComms/GroundComms.java
@@ -9,6 +9,8 @@ import fr.dgac.ivy.Ivy;
 import fr.dgac.ivy.IvyClient;
 import fr.dgac.ivy.IvyException;
 import fr.dgac.ivy.IvyMessageListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,21 +30,55 @@ public class GroundComms {
             @Override
             public void receive(IvyClient ic, String[] strings) {
                 String ack = strings[0];
+                String[] arrayACK = ack.split(" ");
+                String message = "";
                 
-                String ackMessage = "ACK = " + ack;
+                if(arrayACK[1] == "WILCO")message = "Instruction understood and will be complied with.";
+                else if(arrayACK[1] == "UNABLE")message = "Instruction cannot be complied with.";
+                else if(arrayACK[1] == "STANDBY")message = "A response will be delivered shortly.";
                 
                 try {
-                    bus.sendMsg(ackMessage);
+                    bus.sendMsg(message);
                 }catch (IvyException ex){
                     Logger.getLogger(GroundComms.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            
-            
+                }   
             }
         });
         
         //      bind APDLC Format
-
+        bus.bindMsg("^TrackMovedEvent(.*)", new IvyMessageListener() {
+            // callback
+            @Override
+            public void receive(IvyClient client, String[] strings) {
+                
+                String textReceived = strings[0];
+                String[] arrayReceived = textReceived.split(" ");
+                List <String> trafficStatus = new ArrayList <String>();
+                
+                //flightOperator = the arguments for flight status
+                //statusValue = the value of the operators
+                
+                for(int i = 0; i < arrayReceived.length; i++){
+                    String flightOperator = arrayReceived[i].substring(0,arrayReceived[i].indexOf("=") + 1);
+                    
+                    if(arrayReceived[i].indexOf("=") != -1){
+                        flightOperator = arrayReceived[i].substring(0,arrayReceived[i].indexOf("="));
+                    }
+                    
+                    String statusValue = arrayReceived[i].substring(arrayReceived[i].indexOf("=") + 1);
+                    System.out.print(flightOperator + ": " + statusValue + "; ");
+                    trafficStatus.add(flightOperator + ": " + statusValue);
+                }
+                
+                System.out.println();
+                
+                try {
+                    bus.sendMsg(trafficStatus.toString());
+                } catch (IvyException ex) {
+                    Logger.getLogger(AirborneComms.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
         
         //      send APDLC Format
         
