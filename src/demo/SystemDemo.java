35334ecb741aec8b7455fd88827374a87a44b8a8
Mateusz Wiza <43853451+mateuszwiza@users.noreply.github.com>
2022-04-20 01:10:43 +0200

    New demo

--- src/demo/SystemDemo.java
+++ src/demo/SystemDemo.java
@@ -6,57 +6,72 @@ package demo;
 
 import airborne.AirborneComms;
 import fr.dgac.ivy.IvyException;
-import ground.GroundComms;
+import ground.GroundProcessor;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author mateu
  */
 public class SystemDemo {
-    public static void main(String[] args) throws IvyException {
-        GroundComms groundSystem = new GroundComms();
+    public static void main(String[] args) throws IvyException, InterruptedException {
+        GroundProcessor groundSystem = new GroundProcessor();
         AirborneComms selfTraffic = new AirborneComms();
         
-        //groundSystem.stop();
-        //selfTraffic.stop();
-       
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(SystemDemo.class.getName()).log(Level.SEVERE, null, ex);
+        TimeUnit.SECONDS.sleep(20);
+        
+        groundSystem.altitude(290);
+        
+        TimeUnit.SECONDS.sleep(20);
+        
+        groundSystem.speed(540);
+        
+        TimeUnit.SECONDS.sleep(20);
+        
+        groundSystem.heading(80);
+        
+        TimeUnit.SECONDS.sleep(120);
+        
+        //roundSystem.altitude(250);
+        
+        //TimeUnit.SECONDS.sleep(100);
+        
+        groundSystem.stop();
+        selfTraffic.stop();
+        
+        //groundSystem.sendToAirborne("NewFlight NewF NewID=101");
+        
+        
+        /*
+        String[] commands = new String[13];
+        
+        commands[0] = "UM67 PROCEED BACK ON ROUTE";
+        commands[1] = "UM20 CLIMB TO 300";
+        commands[2] = "UM20 DESCEND TO 300";
+        commands[3] = "UM19 MAINTAIN 300";
+        commands[4] = "UM74 PROCEED DIRECT TO XAMPO";
+        commands[5] = "UM94 TURN LEFT HEADING 20";
+        commands[6] = "UM94 TURN RIGHT HEADING 20";
+        commands[7] = "UM106 MAINTAIN 240";
+        commands[8] = "UM111 INCREASE SPEED TO 240";
+        commands[9] = "UM113 REDUCE SPEED TO 240";
+        commands[10] = "UM190 FLY HEADING 20";
+        commands[11] = "UM215 FLY HEADING LEFT 20";
+        commands[12] = "UM215 FLY HEADING RIGHT 20";
+        
+        for (String command : commands) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SystemDemo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println(command);
+            groundSystem.sendToAirborne(command);
         }
+        groundSystem.stop();
+        selfTraffic.stop();
+
+*/
         
-        System.out.println();
-        System.out.println("Requesting Flight Level 300");
-        String cpdlc = "UM20 CLIMB TO 300";
-        System.out.println(cpdlc);
-        groundSystem.sendToAirborne(cpdlc);
-        
-        try {
-            TimeUnit.SECONDS.sleep(15);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(SystemDemo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-         System.out.println();
-        System.out.println("Requesting Heading 80");
-        cpdlc = "UM190 FLY HEADING 80";
-        System.out.println(cpdlc);
-        groundSystem.sendToAirborne(cpdlc);
-        
-        try {
-            TimeUnit.SECONDS.sleep(40);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(SystemDemo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        System.out.println();
-        System.out.println("Requesting Flight Level 200");
-        cpdlc = "UM23 DESCEND TO 200";
-        System.out.println(cpdlc);
-        groundSystem.sendToAirborne(cpdlc);
     }
 }
